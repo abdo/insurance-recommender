324c5961aaed3c3620f25e5fb93461851a762984
Abdo <abdopoetry@gmail.com>
2020-01-26 11:20:55 +0200

    Add main questionnaire parts

---
+++ src/constants/mainQuestions.js
@@ -0,0 +1,44 @@
+import occupationStates from './occupationStates';
+
+const mainQuestions = [
+  {
+    key: 'firstName',
+    text: "What's your first name?",
+    type: 'text',
+  },
+  {
+    key: 'address',
+    text: "What's your address?",
+    type: 'text',
+  },
+  {
+    key: 'occupation',
+    text: "What's your occupation?",
+    type: 'radio',
+    options: occupationStates,
+    required: true,
+  },
+  {
+    key: 'childrenPossession',
+    text: 'Do you have any children?',
+    type: 'radio',
+    options: [
+      { label: 'Yes', value: true },
+      { label: 'No', value: false },
+    ],
+  },
+  {
+    key: 'childrenNumber',
+    text: 'How many children do you have?',
+    type: 'number',
+  },
+  {
+    key: 'email',
+    text: "What's your email?",
+    type: 'number',
+    validation: 'email',
+    required: true,
+  },
+];
+
+export default mainQuestions;
---
+++ src/constants/occupationStates.js
@@ -0,0 +1,16 @@
+const occupationStates = [
+  {
+    label: 'Employed',
+    value: 'EMPLOYED',
+  },
+  {
+    label: 'Self Employed',
+    value: 'SELF_EMPLOYED',
+  },
+  {
+    label: 'Student',
+    value: 'STUDENT',
+  },
+];
+
+export default occupationStates;
