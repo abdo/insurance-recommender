202a0c63846228483e62664cfcfad1a0ca44a967
Abdo <abdopoetry@gmail.com>
2020-01-26 14:46:08 +0200

    Add verification to questionnaire

--- src/pages/questionnaire/index.js
+++ src/pages/questionnaire/index.js
@@ -13,9 +13,17 @@ const Questionnaire = () => {
   );
 
   const currentQuestion = mainQuestions[currentQuestionIndex];
+  const {
+    text,
+    label,
+    required,
+    validations,
+    key: currentQuestionKey,
+    type: inputType = 'text',
+    options,
+  } = currentQuestion;
 
   const isValidInput = () => {
-    const { key: currentQuestionKey } = currentQuestion;
     const enteredValue = answers[currentQuestionKey];
     const { required, validations = [] } = currentQuestion;
 
@@ -32,29 +40,68 @@ const Questionnaire = () => {
   };
 
   const onInputChange = (value) => {
-    const { key: currentQuestionKey } = currentQuestion;
     setAnswers({ ...answers, [currentQuestionKey]: value });
   };
 
   const isLastQuestion = currentQuestionIndex === mainQuestions.length - 1;
   const actionButtonText = isLastQuestion ? 'Submit' : 'Next';
-  const onNext = () => {
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  const onNext = ({ steps = 1 }) => {
+    if (!isValidInput()) {
+      return;
+    }
+    const nextQuestionIndex = currentQuestionIndex + steps;
+    const { dependency: nextQuestionDependency } = mainQuestions[
+      nextQuestionIndex
+    ];
+
+    const dependencyAnswer = answers[nextQuestionDependency];
+    if (
+      nextQuestionDependency &&
+      (!dependencyAnswer || dependencyAnswer === 'false')
+    ) {
+      onNext({ steps: steps + 1 });
+      return;
+    }
+
+    setCurrentQuestionIndex(nextQuestionIndex);
   };
   const onSubmit = () => {};
   const onClickActionButton = isLastQuestion ? onSubmit : onNext;
 
-  const { text, label, required, validations } = currentQuestion;
+  const getInput = () => {
+    if (inputType === 'radio') {
+      return (
+        <>
+          {options.map(({ label, value }) => (
+            <div className='radioInputContainer' key={value}>
+              <input
+                type='radio'
+                name={currentQuestionKey}
+                value={value}
+                onChange={(e) => onInputChange(e.target.value)}
+              />
+              <p>{label}</p>
+            </div>
+          ))}
+        </>
+      );
+    }
+
+    return (
+      <input
+        className='mainTextInput'
+        placeholder={label}
+        type={inputType}
+        value={answers[currentQuestionKey]}
+        onChange={(e) => onInputChange(e.target.value)}
+      />
+    );
+  };
 
   return (
     <div className='pageContainer'>
       <p className='questionText'>{`${required ? '*' : ''}${text}`}</p>
-      <input
-        className='mainTextInput'
-        placeholder={label}
-        onChange={(e) => onInputChange(e.target.value)}
-      />
-
+      {getInput()}
       {validations && `Please enter a valid ${label}`}
 
       <button
